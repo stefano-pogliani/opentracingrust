44bd97312d168709de39b598b2a0c7b9de7119d5
Stefano Pogliani <stefano@spogliani.net>
2018-01-30 19:39:56 +0000

    Implement NoopTracer

--- src/tracers/noop.rs
+++ src/tracers/noop.rs
@@ -2,7 +2,6 @@ use std::sync::mpsc;
 
 use rand::random;
 
-use super::super::Error;
 use super::super::ImplContextBox;
 use super::super::Result;
 
@@ -21,22 +20,47 @@ use super::super::Tracer;
 use super::super::TracerInterface;
 
 
-/// TODO
+/// A tracer that discards spans.
+///
+/// Like for other tracers, spans are still collected in full and are cost memory
+/// until they are read from the `FinishedSpan` receiver and dropped.
+///
+/// Unlike other tracers, the `NoopTracer` will not propagate or extract tracing
+/// information and will discard all `FinishedSpan`s when they are `NoopTracer::report`ed.
+///
+/// This is especially useful as a way to effectively disable tracing in applications built
+/// with OpenTracing support when the end user does not wish to collect tracing information.
+///
+/// # Examples
+///
+/// ```
+/// extern crate opentracingrust;
+///
+/// use opentracingrust::tracers::NoopTracer;
+///
+///
+/// fn main() {
+///     let (tracer, receiver) = NoopTracer::new();
+///     // ... snip ...
+/// }
+/// ```
 pub struct NoopTracer {
     sender: SpanSender
 }
 
 impl TracerInterface for NoopTracer {
     fn extract(&self, _fmt: ExtractFormat) -> Result<Option<SpanContext>> {
-        Err(Error::Msg(String::from("TODO")))
+        // This method intentionality does nothing.
+        Ok(None)
     }
 
     fn inject(&self, _context: &SpanContext, _fmt: InjectFormat) -> Result<()> {
-        Err(Error::Msg(String::from("TODO")))
+        // This method intentionality does nothing.
+        Ok(())
     }
 
     fn span(&self, name: &str, options: StartOptions) -> Span {
-        let trace_id = random::<u64>();
+        let trace_id = random::<[u8; 16]>();
         let span_id = random::<u64>();
         let context = SpanContext::new(ImplContextBox::new(NoopTracerContext {
             trace_id,
@@ -47,7 +71,7 @@ impl TracerInterface for NoopTracer {
 }
 
 impl NoopTracer {
-    /// TODO
+    /// Instantiate a new `NoopTracer`.
     pub fn new() -> (Tracer, SpanReceiver) {
         let (sender, receiver) = mpsc::channel();
         let tracer = NoopTracer { sender };
@@ -55,16 +79,19 @@ impl NoopTracer {
     }
 
 
-    /// TODO
-    pub fn report(_span: FinishedSpan) {
-        // TODO
+    /// "Reports" the finished span into nowere.
+    ///
+    /// `FinishedSpan`s passed to this method are simply dropped.
+    /// This method is to provide a common interface with other tracers.
+    pub fn report(span: FinishedSpan) {
+        drop(span);
     }
 }
 
-/// TODO
+/// Inner NoopTracer context.
 #[derive(Clone, Debug)]
 struct NoopTracerContext {
-    trace_id: u64,
+    trace_id: [u8; 16],
     span_id: u64
 }
 
@@ -86,5 +113,52 @@ impl SpanReferenceAware for NoopTracerContext {
 
 #[cfg(test)]
 mod tests {
-    // TODO
+    mod inner_context {
+        use super::super::super::super::ImplContextBox;
+        use super::super::super::super::SpanContext;
+        use super::super::super::super::SpanReference;
+        use super::super::super::super::SpanReferenceAware;
+
+        use super::super::NoopTracer;
+        use super::super::NoopTracerContext;
+
+        #[derive(Clone)]
+        struct OtherContext {}
+        impl SpanReferenceAware for OtherContext {
+            fn reference_span(&mut self, _: &SpanReference) {}
+        }
+
+        #[test]
+        fn child_of_updates_trace_id() {
+            let (tracer, _) = NoopTracer::new();
+            let parent = tracer.span("test1");
+            let mut span = tracer.span("test2");
+            span.child_of(parent.context().clone());
+
+            let inner_parent = parent.context().impl_context::<NoopTracerContext>().unwrap();
+            let inner_span = span.context().impl_context::<NoopTracerContext>().unwrap();
+            assert_eq!(inner_parent.trace_id, inner_span.trace_id);
+        }
+
+        #[test]
+        fn follows_updates_trace_id() {
+            let (tracer, _) = NoopTracer::new();
+            let parent = tracer.span("test1");
+            let mut span = tracer.span("test2");
+            span.follows(parent.context().clone());
+
+            let inner_parent = parent.context().impl_context::<NoopTracerContext>().unwrap();
+            let inner_span = span.context().impl_context::<NoopTracerContext>().unwrap();
+            assert_eq!(inner_parent.trace_id, inner_span.trace_id);
+        }
+
+        #[test]
+        #[should_panic(expected = "Unsupported span context, was it created by NoopTracer?")]
+        fn panics_if_invalid_context() {
+            let (tracer, _) = NoopTracer::new();
+            let parent = SpanContext::new(ImplContextBox::new(OtherContext{}));
+            let mut span = tracer.span("test");
+            span.child_of(parent);
+        }
+    }
 }
